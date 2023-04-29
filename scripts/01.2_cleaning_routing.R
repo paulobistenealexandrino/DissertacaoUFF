06e262a309ae8dbf4027ab39a8e0f38c921ef1b9
Paulo Alexandrino <paulobistenealexandrino@gmail.com>
2023-04-29 12:31:25 -0300

    Ajustar GTFS

--- scripts/01.2_cleaning_routing.R
+++ scripts/01.2_cleaning_routing.R
@@ -1,5 +1,9 @@
 ## Cleaning Routing Data ##
 
+library(gtfstools)
+library(tidyverse)
+library(sf)
+
 # Esse script prepara e organiza os arquivos que serão utilizados no
 # calculo da matriz de tempo de viagem.
 
@@ -25,8 +29,7 @@ landuse_rio <- read_rds("input/data_processed/landuse_rio.RDS")
 # Pontos de Origem: Locais com população entre 15 e 69 anos maior que zero.
 
 origin_points <- landuse_rio %>%
-  mutate(PIA = rowSums(across(P012:P015))) %>%
-  filter(PIA > 0) %>%
+  filter(P001 > 0) %>%
   select(id = id_hex, lon, lat) %>%
   st_drop_geometry()
 
@@ -35,7 +38,7 @@ write.csv(origin_points, "input/data_raw/r5r/origin_points.csv", row.names = FAL
 # Pontos de Destino: Locais com número de oportunidades de emprego maior que zero.
 
 destiny_points <- landuse_rio %>%
-  filter(T004 > 0) %>%
+  filter(T001 > 0 | E001 > 0 | S001 > 0) %>%
   select(id = id_hex, lon, lat) %>%
   st_drop_geometry()
 
@@ -43,3 +46,15 @@ write.csv(destiny_points, "input/data_raw/r5r/destiny_points.csv", row.names = F
 
 
 ### GTFS
+folder <- "input/data_raw/r5r/"
+
+# Lendo GTFS
+gtfs_smtr <- read_gtfs(file.path(folder,"gtfs_rio-de-janeiro.zip"))
+gtfs_setrans <- read_gtfs(file.path(folder,"gtfs_setrans.zip"))
+
+# Unindo GTFS
+gtfs_rj <- merge_gtfs(gtfs_smtr, gtfs_setrans)
+
+# Salvando
+endereco_destino <- paste0(folder,"gtfs_rj_merged.zip")
+write_gtfs(gtfs_rj, path = endereco_destino)
